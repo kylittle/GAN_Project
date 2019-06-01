238a1b1db17f56268eba8b2ddb152a476b05cdaa
Aniranth <aniranth@gmail.com>
2019-05-31 23:53:03 -0700

    Improved algorithms and updated samples

--- delta_extract.py
+++ delta_extract.py
@@ -39,35 +39,36 @@ def main():
 def read_ini_config():
     global epoch_count, output_frequency, mode #Let the setting variables be Global
     config = configparser.ConfigParser()
-    config.read("./config.ini")
-    epoch_count = int(config["DEFAULT"]["EpochCount"])
+    config.read("./config.ini") #Get configuration file settings
+    epoch_count = int(config["DEFAULT"]["EpochCount"]) #Read Settings from the config var
     output_frequency = int(config["DEFAULT"]["EpochsBeforeOutput"])
     mode = int(config["DEFAULT"]["TrainingType"])
-    mode = 0 if mode != 0 and mode != 1 else mode
+    mode = 0 if mode != 0 and mode != 1 else mode #Default to bach if mode is not properly set
 
 #Input: MidiFile Object, Output: Ordered Matrix with each part representing a note
 def make_midi_matrix(mid_in, data_out):
     global min_time, max_time, min_channel, max_channel, min_note, max_note, min_attack, max_attack
-    first_loop = True
+    first_loop = True #If we are in the first loop we will say the note is zero and store the note to get the difference 
     for track in mid_in.tracks:
         for msg in track:
             if not first_loop:
                 if not msg.is_meta and msg.type == "note_on":
-                    command = (msg.channel, msg.note-prev_note, msg.velocity, msg.time)
+                    command = (msg.channel, msg.note-prev_note, msg.velocity, msg.time) #Get the note difference
                     min_time = min_time if min_time <= msg.time else msg.time
                     max_time = max_time if max_time >= msg.time else msg.time
                     min_channel = min_channel if min_channel <= msg.channel else msg.channel
                     max_channel = max_channel if max_channel >= msg.channel else msg.channel
                     data_out.append(command)
+                    prev_note = msg.note
             else:
                 if not msg.is_meta and msg.type == "note_on":
-                    command = (msg.channel, 0, msg.velocity, msg.time)
+                    command = (msg.channel, 0, msg.velocity, msg.time) #Init line one with a note base of 0
                     first_loop = False
                     min_time = min_time if min_time <= msg.time else msg.time
                     max_time = max_time if max_time >= msg.time else msg.time
                     min_channel = min_channel if min_channel <= msg.channel else msg.channel
                     max_channel = max_channel if max_channel >= msg.channel else msg.channel
-                    prev_note = msg.note
+                    prev_note = msg.note #Save the current cycles note for differences
     return data_out
 
 def unscale_midi(data_set):
@@ -75,7 +76,7 @@ def unscale_midi(data_set):
     first_loop = True
     for unscale_data_line in data_set:
         if first_loop:
-            unscale_data_line = (round(unscale(unscale_data_line[0], min_channel, max_channel)), 60, 
+            unscale_data_line = (round(unscale(unscale_data_line[0], min_channel, max_channel)), 36, 
                 round(unscale(unscale_data_line[2], min_attack, max_attack)), round(unscale(unscale_data_line[3], min_time, max_time)))
             prev_note = 60
             first_loop= False
@@ -90,7 +91,7 @@ def unscale_midi(data_set):
 
 def scale_for_learning(data_set):
     return_data_set = []
-    #First scale channel (this may not be something we need to do
+    #First scale channel (this may not be something we need to do)
     for data_line in data_set:
         scaled_data = (scale(data_line[0], min_channel, max_channel), scale(data_line[1], min_note, max_note), 
             scale(data_line[2], min_attack, max_attack), scale(data_line[3], min_time, max_time))
@@ -245,15 +246,54 @@ def save_midi_output(epoch, generator, examples=2400):
     track1=MidiTrack()
     track2=MidiTrack()
     track3=MidiTrack()
+    track4=MidiTrack()
+    track5=MidiTrack()
+    track6=MidiTrack()
+    track7=MidiTrack()
+    track8=MidiTrack()
+    track9=MidiTrack()
+    track10=MidiTrack()
+    track11=MidiTrack()
+
     for data_line in generated:
-        if data_line[0] == 0:
-            track0.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+200))
-        if data_line[0] == 1:
-            track1.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+200))
-        if data_line[0] == 2:
-            track2.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+200))
-        if data_line[0] == 3:
-            track3.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+200))
+        if data_line[1] < 108 and data_line[1] > 21:
+            if data_line[0] == 0:
+                track0.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+110)) #+200?
+                track0.append(Message('note_off'))
+            if data_line[0] == 1:
+                track1.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+110)) #+200?
+                track1.append(Message('note_off'))
+            if data_line[0] == 2:
+                track2.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+110)) #+200?
+                track2.append(Message('note_off'))
+            if data_line[0] == 3:
+                track3.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+110)) #+200?
+                track3.append(Message('note_off'))
+            if data_line[0] == 4:
+                track4.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+110)) #+200?
+                track4.append(Message('note_off'))
+            if data_line[0] == 5:
+                track5.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+110)) #+200?
+                track5.append(Message('note_off'))
+            if data_line[0] == 6:
+                track6.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+110)) #+200?
+                track6.append(Message('note_off'))
+            if data_line[0] == 7:
+                track7.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+110)) #+200?
+                track7.append(Message('note_off'))
+            if data_line[0] == 8:
+                track8.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+110)) #+200?
+                track8.append(Message('note_off'))
+            if data_line[0] == 9:
+                track9.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+110)) #+200?
+                track9.append(Message('note_off'))
+            if data_line[0] == 10:
+                track10.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+110)) #+200?
+                track10.append(Message('note_off'))
+            if data_line[0] == 11:
+                track11.append(Message('note_on', note=int(abs(data_line[1])), velocity=int(abs(data_line[2])), time=int(abs(data_line[3]))+110)) #+200?
+                track11.append(Message('note_off'))
+ 
     print(generated)
     mid.tracks.append(track0)
     mid.tracks.append(track1)
@@ -262,16 +302,16 @@ def save_midi_output(epoch, generator, examples=2400):
     print('The generated track has been saved to output.mid')
     mid.save('./output/output' + str(epoch) + '.mid')
 
-def train(epochs=1, batch_size=128):
-    x_train = load_midi_data()
+def train(epochs=1, batch_size=128): #These default parameters are largely ignored
+    x_train = load_midi_data() #Load the data from the inputs folder
     batch_count = int(x_train.shape[0]/batch_size)
     
     adam = get_optimizer()
     generator = get_generator(adam)
     discriminator = get_discriminator(adam)
-    gan = get_gan_network(discriminator, random_dim, generator, adam)
+    gan = get_gan_network(discriminator, random_dim, generator, adam) #Build our gan
 
-    for e in range(1, epochs+1):
+    for e in range(1, epochs+1): #Begin model training
         print('-'*15, 'Epoch %d' %e, '-'*15)
         for obj in tqdm(range(batch_count)):
             noise = np.random.normal(0,1,size=[batch_size, random_dim])
@@ -291,7 +331,7 @@ def train(epochs=1, batch_size=128):
             discriminator.trainable = False
             gan.train_on_batch(noise, y_gen)
 
-        if e == epoch_count or e % output_frequency == 0:
+        if e == epoch_count or e % output_frequency == 0: #Output every frequency cycles
             #history = gan.fit()
             #plt.plot(history.history['loss'])
             #plt.plot(history.history['val_loss'])
